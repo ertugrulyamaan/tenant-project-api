7d7938f9096202b303ab79cf19434467b8c3c63e
Ertu <ertu@Ertu-Mac-mini.local>
2026-03-15 22:55:38 +0300

    chore: add Prisma client and adapter dependencies, update package-lock for new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
     AuthModule,
   ],
   controllers: [AppController],
